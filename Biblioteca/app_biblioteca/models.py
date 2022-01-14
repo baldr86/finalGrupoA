c5f0c4897c7c0b1daf9333ae9afb6601c80b348d
balder86 <baldrtec@gmail.com>
2022-01-13 22:56:13 -0300

    primera estructura con borrador de models y primer template de inicio

--- Biblioteca/app_biblioteca/models.py
+++ Biblioteca/app_biblioteca/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+class Libro(models.Model):
+    nombre= models.CharField(max_length=40)
+    autor= models.CharField(max_length=40)
+    publicacion= models.DateField()
+    genero= models.CharField(max_length=20)
+   
+
+class Autor(models.Model):
+    nombre= models.CharField(max_length=40)
+    apellido= models.CharField(max_length=40)
+    nacionalidad= models.CharField(max_length=40)
+    cantLibros= models.IntegerField()
+
+class Genero(models.Model):
+    generos=["Aventura", "Suspenso", "historia"]
+    genero= genero= models.CharField(max_length=20)
+
+
