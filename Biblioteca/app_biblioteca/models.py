c8e05d1b2d93d06c3806a7a54d9f35b49a50cc04
PaoPepi <penderle.pe@gmail.com>
2022-01-15 20:24:40 -0300

    Cursos

--- Biblioteca/app_biblioteca/models.py
+++ Biblioteca/app_biblioteca/models.py
@@ -7,3 +7,9 @@ class Libro(models.Model):
     genero= models.CharField(max_length=20)
     editorial=models.CharField(max_length=20)
 
+class Cursos(models.Model):
+    nombre=models.CharField(max_length=40)
+    codigocurso=models.IntegerField()
+    docente=models.CharField(max_length=30)
+    diahorario=models.CharField(max_length=40)
+
--- Biblioteca/app_biblioteca/urls.py
+++ Biblioteca/app_biblioteca/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
+from ProyectoGrupoA.finalGrupoA.Biblioteca.app_biblioteca.models import Cursos
+from ProyectoGrupoA.finalGrupoA.Biblioteca.app_biblioteca.views import curso
 from app_biblioteca import views
 
 urlpatterns = [
     path('', views.inicio),
-    path("libros.html", views.libros)
-    
+    path("libros.html", views.libros),
+    path("cursos.html", views.curso)
 ]
